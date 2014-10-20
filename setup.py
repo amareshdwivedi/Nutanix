8661348b41c1455265fb162429498febc34a475e
smourya <sunny.mourya@xoriant.com>
2014-10-20 12:50:18 +0530

    Change description: Removed hardcoded config file names
    Unit test details: Tested by creating .egg file

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ Created on Oct 14, 2014
 '''
 
 
-from setuptools import setup, find_packages
-from distutils.extension import Extension
+from setuptools import setup 
 requires = ['reportlab >= 3.1.8',
           'pyvmomi >= 5.5.0.2014.1.1',
           'prettytable >= 0.7.2',
@@ -17,11 +16,9 @@ requires = ['reportlab >= 3.1.8',
 setup(
     author='krishnamurthy_b',
     name='HealthCheck',
-     version = '1.0.0',
+    version = '1.0.0',
     packages=['src','src.checkers','src.reports'],
-    include_package_data=True,
-    data_files=[('src', ['src//nutanixlogo.png']),
-                  ('src//conf', ['src//conf//ncc.conf','src//conf//vc.conf','src//conf//auth.conf'])],
+    package_data ={'' : ['*.png','conf//*.conf']},
     long_description=open('README.md').read(),
     install_requires=requires
       )
