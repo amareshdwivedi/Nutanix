4bc216363c3e1da5c60ce71db444c1e790182077
smourya <sunny.mourya@xoriant.com>
2014-10-21 16:09:57 +0530

    Change description: Made the setup file compatible with both bdist(for creating .egg) and sdist(for creating .zip) commands.
    Unit test details: Tested by creating .egg and .zip file

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
-'''
-Created on Oct 14, 2014
-
-@author: krishnamurthy_b
-'''
-
-
-from setuptools import setup 
-requires = ['reportlab >= 3.1.8',
+from setuptools import setup, find_packages
+requires = [
+          'reportlab >= 3.1.8',
           'pyvmomi >= 5.5.0.2014.1.1',
           'prettytable >= 0.7.2',
           'paramiko >=1.15.1',
           'pycrypto >= 2.6.1',
-          'colorama >=0.3.2',],
+          'colorama >=0.3.2',
+          ],
 
 setup(
-    author='krishnamurthy_b',
+    author='nutanix',
     name='HealthCheck',
     version = '1.0.0',
-    packages=['src','src.checkers','src.reports'],
+	packages=find_packages(),
     package_data ={'' : ['*.png','conf//*.conf']},
-    long_description=open('README.md').read(),
     install_requires=requires
       )
     
