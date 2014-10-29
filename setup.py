6e0a9b926f582e4392a9e534330e9c8605d7b29f
System <barathi.krishnamurthy@xoriant.com>
2014-10-29 15:18:06 +0530

    @Anand : In PDFGenerator, changed Images and Colors. Added resources/images folder, and updated Setup.py
    Unit Test: on checks run, generate required check in PDF with images, color and fonts

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='HealthCheck',
     version = '1.0.0',
 	packages=find_packages(),
-    package_data ={'' : ['*.png','conf//*.conf','bin//*.sh','bin//*.bat']},
+    package_data ={'' : ['conf//*.conf','bin//*.sh','bin//*.bat','resources//images//*.png']},
     exclude_package_data = { '': ['bin//build.bat'] },
     install_requires=requires,
     zip_safe=False
