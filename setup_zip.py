0b2681315ad3f580a5b63a2ab4f878be2ca40d31
Anand Nevase <sagar.gaikwad@xoriant.com>
2014-10-31 23:10:37 +0530

    Description: comment out code and add functionality to download from internet as it is failing for 64bit machines
    Unit test : install.py - install all dependency required for Healthcheckup and run_health_check will in site-packages folder

--- setup_zip.py
+++ setup_zip.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
+requires = [
+          'reportlab >= 3.1.8',
+          'pyvmomi >= 5.5.0.2014.1.1',
+          'prettytable >= 0.7.2',
+          'paramiko >=1.15.1',
+          'pycrypto >= 2.6.1',
+          'colorama >=0.3.2',
+          ],
+          
 setup(
     author='nutanix',
     name='HealthCheck',
     version = '1.0.0',
-	packages=find_packages(),
+    packages=find_packages(),
     package_data ={'' : ['conf//*.conf','bin//*.sh','bin//*.bat','resources//images//*.png']},
     exclude_package_data = { '': ['bin//build*.bat'] },
+    install_requires=requires,
     zip_safe=False
       )
     
