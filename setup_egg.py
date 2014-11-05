46c5cbd41125152a2b20e0c59547e01d604ee9a7
smourya <sunny.mourya@xoriant.com>
2014-11-05 14:52:15 +0530

    changes done to include web.py package in setup and install scripts

--- setup_egg.py
+++ setup_egg.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 requires = [
-          'reportlab >= 3.1.8',
-          'pyvmomi >= 5.5.0.2014.1.1',
+          'colorama >=0.3.2', 
           'prettytable >= 0.7.2',
           'paramiko >=1.15.1',
-          'pycrypto >= 2.6.1',
-          'colorama >=0.3.2',
+          'pyvmomi >= 5.5.0.2014.1.1',
+          'reportlab >= 3.1.8',
           ],
 		  
 setup(
@@ -13,7 +12,7 @@ setup(
     name='HealthCheck',
     version = '1.0.0',
 	packages=find_packages(),
-    package_data ={'' : ['conf//*.conf','bin//*.sh','bin//*.bat','resources//images//*.png']},
+    package_data ={'' : ['conf//*.conf','bin//*.sh','bin//*.bat','static//fonts//*.*','static//images//*.*','static//js//*.*','static//styles//*.*','templates//*.*']},
     exclude_package_data = { '': ['bin//build*.bat'] },
     install_requires=requires,
     zip_safe=False
