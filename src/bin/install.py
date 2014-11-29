e1b81d728bb683b0cd5951e1150040d02734172a
Sunny Mourya <sunny.mourya@xoriant.com>
2014-11-29 18:40:46 +0530

    Change Details : Installation failure message displayed on screen

--- src/bin/install.py
+++ src/bin/install.py
@@ -20,6 +20,7 @@ else :
     default_install+=os.path.sep+'healthcheck'
     os.makedirs(default_install)    
 
+returncode = 0
 print "Starting HealthCheck Installation..."
 def progress_bar():
     for i in range(21):
@@ -30,10 +31,14 @@ def progress_bar():
     sys.stdout.write('\r')
         
 def installing_dependencies():
-    with open("install_log.log", "w+") as output:
-        install_helper=os.path.abspath(os.path.dirname(__file__))+os.path.sep +'scripts'+ os.path.sep +"install_helper.py"
-        subprocess.call(["python", install_helper,default_install],stderr=output,stdout=output);
-
+    try:
+        with open("install_log.log", "w+") as output:
+            install_helper=os.path.abspath(os.path.dirname(__file__))+os.path.sep +'scripts'+ os.path.sep +"install_helper.py"
+            subprocess.check_call(["python", install_helper,default_install],stderr=output,stdout=output);
+    except subprocess.CalledProcessError as cpe:
+        global returncode
+        returncode = cpe.returncode
+        
 threads = []
 thread_installing_dep = threading.Thread(target=installing_dependencies)
 thread_progress_bar = threading.Thread(target=progress_bar)
@@ -43,78 +48,81 @@ threads.append(thread_progress_bar)
 threads.append(thread_installing_dep)
 for t in threads:
     t.join()
+
+if returncode == 0:
     
-print "Creating healthcheck script..."
+    print "Creating healthcheck script..."
 
-healthchecklines=["#!"+sys.executable+"\n" if sys.platform.startswith("linux") else '','import os,sys',
-'lib_path = ' + "'" + default_install + "'",
-'os.environ["PYTHONPATH"] = lib_path',
-'if not os.path.exists(os.getcwd() + os.path.sep +"reports"):',
-'     os.mkdir("reports")',
-'executable_path="python "+lib_path+os.path.sep+"HealthCheck-1.0.0-py2.7.egg"+os.path.sep+"src"+os.path.sep+"health_check.pyc "',
-'os.system(executable_path+ " ".join(sys.argv[1:]))']
+    healthchecklines=["#!"+sys.executable+"\n" if sys.platform.startswith("linux") else '','import os,sys',
+    'lib_path = ' + "'" + default_install + "'",
+    'os.environ["PYTHONPATH"] = lib_path',
+    'if not os.path.exists(os.getcwd() + os.path.sep +"reports"):',
+    '     os.mkdir("reports")',
+    'executable_path="python "+lib_path+os.path.sep+"HealthCheck-1.0.0-py2.7.egg"+os.path.sep+"src"+os.path.sep+"health_check.pyc "',
+    'os.system(executable_path+ " ".join(sys.argv[1:]))']
 
-health_check_pyfile=open(default_install+os.path.sep+"healthcheck.py","wb")
-for line in healthchecklines:
-    health_check_pyfile.writelines(line+"\n")
-health_check_pyfile.close()
-time.sleep(2)
+    health_check_pyfile=open(default_install+os.path.sep+"healthcheck.py","wb")
+    for line in healthchecklines:
+        health_check_pyfile.writelines(line+"\n")
+    health_check_pyfile.close()
+    time.sleep(2)
 
-print "Creating webhealthcheck script..."
+    print "Creating webhealthcheck script..."
 
-webhealthchecklines=["#!"+sys.executable+"\n" if sys.platform.startswith("linux") else '','import os,sys',
-'lib_path = ' + "'" + default_install + "'",
-'os.environ["PYTHONPATH"] = lib_path',
-'cur_dir = os.getcwd()',
-'if not os.path.exists(cur_dir + os.path.sep +"reports"):',
-'     os.mkdir("reports")',
-'os.chdir(lib_path+os.path.sep+"HealthCheck-1.0.0-py2.7.egg"+os.path.sep+"src"+os.path.sep)',
-'executable_path="python web_health_check.pyc 8080 " + cur_dir',
-'os.system(executable_path)']
+    webhealthchecklines=["#!"+sys.executable+"\n" if sys.platform.startswith("linux") else '','import os,sys',
+    'lib_path = ' + "'" + default_install + "'",
+    'os.environ["PYTHONPATH"] = lib_path',
+    'cur_dir = os.getcwd()',
+    'if not os.path.exists(cur_dir + os.path.sep +"reports"):',
+    '     os.mkdir("reports")',
+    'os.chdir(lib_path+os.path.sep+"HealthCheck-1.0.0-py2.7.egg"+os.path.sep+"src"+os.path.sep)',
+    'executable_path="python web_health_check.pyc 8080 " + cur_dir',
+    'os.system(executable_path)']
 
-web_health_check_pyfile=open(default_install+os.path.sep+"webhealthcheck.py","wb")
-for line in webhealthchecklines:
-    web_health_check_pyfile.writelines(line+"\n")
-web_health_check_pyfile.close()
-time.sleep(2)
+    web_health_check_pyfile=open(default_install+os.path.sep+"webhealthcheck.py","wb")
+    for line in webhealthchecklines:
+        web_health_check_pyfile.writelines(line+"\n")
+    web_health_check_pyfile.close()
+    time.sleep(2)
 
-shutil.copy(default_install+os.path.sep+"healthcheck.py",os.path.abspath(os.path.dirname(__file__)))
-shutil.copy(default_install+os.path.sep+"webhealthcheck.py",os.path.abspath(os.path.dirname(__file__)))
+    shutil.copy(default_install+os.path.sep+"healthcheck.py",os.path.abspath(os.path.dirname(__file__)))
+    shutil.copy(default_install+os.path.sep+"webhealthcheck.py",os.path.abspath(os.path.dirname(__file__)))
 
-if sys.platform.startswith("linux"):
-    os.chmod(os.path.abspath(os.path.dirname(__file__))+os.path.sep+"healthcheck.py",0755)
-    os.chmod(os.path.abspath(os.path.dirname(__file__))+os.path.sep+"webhealthcheck.py",0755)
+    if sys.platform.startswith("linux"):
+        os.chmod(os.path.abspath(os.path.dirname(__file__))+os.path.sep+"healthcheck.py",0755)
+        os.chmod(os.path.abspath(os.path.dirname(__file__))+os.path.sep+"webhealthcheck.py",0755)
 
 
-if sys.platform.startswith("win"):
-    #add to system path
-    print "Setting environment path variable..."
-    subprocess.call(['setx','Path','%Path%;'+default_install])
+    if sys.platform.startswith("win"):
+        #add to system path
+        print "Setting environment path variable..."
+        subprocess.call(['setx','Path','%Path%;'+default_install])
 
-if sys.platform.startswith("linux"):
-    shutil.copy(default_install+os.path.sep+"healthcheck.py","/bin/healthcheck.py")
-    shutil.copy(default_install+os.path.sep+"webhealthcheck.py","/bin/webhealthcheck.py")
-    os.chmod("/bin/healthcheck.py",0755)
-    os.chmod("/bin/webhealthcheck.py",0755)
-    
+    if sys.platform.startswith("linux"):
+        shutil.copy(default_install+os.path.sep+"healthcheck.py","/bin/healthcheck.py")
+        shutil.copy(default_install+os.path.sep+"webhealthcheck.py","/bin/webhealthcheck.py")
+        os.chmod("/bin/healthcheck.py",0755)
+        os.chmod("/bin/webhealthcheck.py",0755)
+        
 
-print "Creating uninstall script..."
+    print "Creating uninstall script..."
 
-uninstall_lines=['import os,sys,shutil,time',
-'print "Starting HealthCheck Un-installation..."',                 
-'install_dir = ' + "'" + default_install + "'",                               
-'shutil.rmtree(install_dir, ignore_errors=True)',
-'os.remove(\'/bin/healthcheck.py\')' if sys.platform.startswith("linux") else '',
-'os.remove(\'/bin/webhealthcheck.py\')' if sys.platform.startswith("linux") else '',
-'time.sleep(2)',
-'print "HealthCheck Un-installation Successfull..."']
+    uninstall_lines=['import os,sys,shutil,time',
+    'print "Starting HealthCheck Un-installation..."',                 
+    'install_dir = ' + "'" + default_install + "'",                               
+    'shutil.rmtree(install_dir, ignore_errors=True)',
+    'os.remove(\'/bin/healthcheck.py\')' if sys.platform.startswith("linux") else '',
+    'os.remove(\'/bin/webhealthcheck.py\')' if sys.platform.startswith("linux") else '',
+    'time.sleep(2)',
+    'print "HealthCheck Un-installation Successfull..."']
 
-uninstall_pyfile=open(default_install+os.path.sep+"uninstall.py","wb")
-for line in uninstall_lines:
-    uninstall_pyfile.writelines(line+"\n")
-uninstall_pyfile.close()
-time.sleep(2)
+    uninstall_pyfile=open(default_install+os.path.sep+"uninstall.py","wb")
+    for line in uninstall_lines:
+        uninstall_pyfile.writelines(line+"\n")
+    uninstall_pyfile.close()
+    time.sleep(2)
 
-print "HealthCheck Installation Successfull..."
+    print "HealthCheck Installation Successfull..."
 
-    
+else:
+    print "Exception occured during installtion process...\nPlease refer install_log for details"        
